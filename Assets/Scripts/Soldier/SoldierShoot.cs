fd0d2379e72a3f69dabb60574bb7c1bb20b7fd81
LeoVirolainen <virolainen.leo@gmail.com>
2020-11-15 14:20:26 +0200

    Character

---
+++ Assets/Scripts/Soldier/SoldierShoot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierShoot : MonoBehaviour {
+    public GameObject projectile;
+    public float shootForce;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            Discharge();
+        }
+    }
+
+    void Discharge() {
+        GameObject newProjectile = Instantiate(projectile, transform.position, transform.rotation);
+        newProjectile.GetComponent<Rigidbody>().AddForce(newProjectile.transform.forward * shootForce, ForceMode.Impulse);
+        Destroy(newProjectile, 5f);
+    }
+}
